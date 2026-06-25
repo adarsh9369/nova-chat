999345023123dd9cf6792381393859f51c75c2e9
adarsh9369 <adarshmishra5736@gmail.com>
2026-06-25 12:50:50 +0530

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,9 +1,9 @@
 window.nebulaFirebaseConfig = {
-  "apiKey": "AIzaSyAs1-DefaultSandboxNebulaKey",
-  "authDomain": "nebulachat-demo.firebaseapp.com",
-  "databaseURL": "https://nebulachat-demo-default-rtdb.firebaseio.com",
-  "projectId": "nebulachat-demo",
-  "storageBucket": "nebulachat-demo.appspot.com",
-  "messagingSenderId": "1234567890",
-  "appId": "1:1234567890:web:abcdef123456"
+  "apiKey": "AIzaSyAtWqGgaxUfLO9Ew-IqBERwLACUxUsSweg",
+  "authDomain": "nova-chat-b444e.firebaseapp.com",
+  "databaseURL": "https://nova-chat-b444e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "nova-chat-b444e",
+  "storageBucket": "nova-chat-b444e.firebasestorage.app",
+  "messagingSenderId": "325505205099",
+  "appId": "1:325505205099:web:0eb638f2b876d2d25e422e"
 };
