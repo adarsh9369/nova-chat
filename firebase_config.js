3cb2047f9ae1b9b342b8d35a8193864be598d0a8
Adarsh mishra <adarshmishra5736@gmail.com>
2026-06-25 11:44:10 +0530

    Add files via upload

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+window.nebulaFirebaseConfig = {
+  "apiKey": "AIzaSyAs1-DefaultSandboxNebulaKey",
+  "authDomain": "nebulachat-demo.firebaseapp.com",
+  "databaseURL": "https://nebulachat-demo-default-rtdb.firebaseio.com",
+  "projectId": "nebulachat-demo",
+  "storageBucket": "nebulachat-demo.appspot.com",
+  "messagingSenderId": "1234567890",
+  "appId": "1:1234567890:web:abcdef123456"
+};
